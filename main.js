a35a06f88eb680b1858cd680a80d58deffdf7a23
Arnar Yngvason <arnar1@gmail.com>
2016-03-31 20:13:28 +0100

    Not depending on bluebird any more

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 "use strict"
 
-var Promise = require('bluebird')
-var passport = require('passport')
 var MockExpressRequest = require('mock-express-request')
 var MockExpressResponse = require('mock-express-response')
 
@@ -18,22 +16,28 @@ module.exports = function deepstreamExpress(middleware, props) {
       })
       var res = new MockExpressResponse({request: req})
 
-      function use(fn) {
-        return new Promise(function(resolve, reject) {
-          return fn(req, res, function(err) {
-            return err ? reject(err) : resolve()
-          })
-        })
-      }
-
-      return Promise.each([].concat(middleware), use).then(function() {
+      function allMiddlewareLoaded() {
         scope.req = req
         scope.res = res
 
         if(props.isValidUser !== undefined) {
           return props.isValidUser.call(scope, connectionData, authData, callback)
         }
+      }
+
+      // Setup sequence:
+      var fnSeq = [].concat(middleware).map(function(fn, i) {
+        return function() {
+          var next = fnSeq[i+1] || allMiddlewareLoaded
+          fn(req, res, function(err) {
+            if(err) {throw err}
+            next()
+          })
+        }
       })
+
+      // Init sequence:
+      fnSeq[0]()
     },
     canPerformAction: props.canPerformAction ? props.canPerformAction.bind(scope) : undefined,
     onClientDisconnect: props.onClientDisconnect ? props.onClientDisconnect.bind(scope) : undefined
