d0bfa21eb3fb2a2c979c819406f1cea935c84f87
Arnar Yngvason <arnar1@gmail.com>
2016-04-02 13:42:17 +0000

    Even simpler

--- main.js
+++ main.js
@@ -4,39 +4,26 @@ var MockExpressRequest = require('mock-express-request')
 var MockExpressResponse = require('mock-express-response')
 
 
-module.exports = function deepstreamExpress(middleware, isValidUser, catchMiddlewareError) {
-  return function(connData, authData, callback) {
-    var self = this
+module.exports = function useExpressMiddleware(middleware, headers, callback) {
+  var req = new MockExpressRequest({
+    method: 'GET',
+    url: '/',
+    headers: headers
+  })
+  var res = new MockExpressResponse({request: req})
 
-    var req = new MockExpressRequest({
-      method: 'GET',
-      url: '/',
-      headers: connData.headers
-    })
-    var res = new MockExpressResponse({request: req})
-
-    // Setup sequence:
-    var fnSeq = [].concat(middleware).map(function(fn, i) {
-      return function(err) {
-        if(err) {return middlewareError(err)}
-        fn(req, res, fnSeq[i+1] || allMiddlewareLoaded)
-      }
-    })
-
-    function middlewareError(err) {
-      if(catchMiddlewareError !== undefined) {
-        return catchMiddlewareError.call(self, err)
-      }
-      throw err
+  // Setup sequence:
+  var fnSeq = [].concat(middleware).map(function(fn, i) {
+    return function(err) {
+      if(err) {return callback(err, req, res)}
+      fn(req, res, fnSeq[i+1] || allMiddlewareLoaded)
     }
+  })
 
-    function allMiddlewareLoaded() {
-      connData.req = req
-      connData.res = res
-      return isValidUser.call(self, connData, authData, callback)
-    }
-
-    // Init sequence:
-    fnSeq[0]()
+  function allMiddlewareLoaded() {
+    callback(null, req, res)
   }
+
+  // Init sequence:
+  fnSeq[0]()
 }
